48ca2f19d45485ef53ceb9639b36a06e51c4da64
svetozar <svetozar.cvele.stojkovic@gmail.com>
2018-01-04 14:43:12 +0100

    [init][l]: * added data/primary-tumor.csv; * added scripts/main.py; * added datapackage.json; * added README.md;

---
+++ README.md
@@ -0,0 +1,32 @@
+This is a dataset about primary tumors in people.
+
+## Data
+
+This dataset was found on [OpenML - primary-tumor](https://www.openml.org/d/171)
+
+This primary tumor domain was obtained from the University Medical Centre, 
+Institute of Oncology, Ljubljana, Yugoslavia. Thanks go to 
+M. Zwitter and M. Soklic for providing the data.
+_Please include this citation if you plan to use this database._
+
+### Output data 
+
+Output data is located in directory `data`
+
+`data/primary-tumor.csv`
+
+Fields in csv are:
+
+
+
+## Scripts
+
+Scripts are in directory `scripts`
+
+`scripts/main.py`
+
+## Licence
+Licensed under the [Public Domain Dedication and License][pddl] (assuming
+either no rights or public domain license in source data).
+
+[pddl]: http://opendatacommons.org/licenses/pddl/1.0/
---
+++ scripts/main.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+
+with open("../data/primary-tumor.csv", "w") as bumps_file:
+    # bumps_file.write("season,age,childish-disease,trauma,surgical-intervention,fevers,alcoholic,"
+    #                  "smoking,sitting,output\n")
+    for line in urlopen("https://www.openml.org/data/get_csv/3594/dataset_113_primary-tumor.arff"):
+        decodedLine = line.decode('UTF-8')
+        decodedLine = decodedLine.replace('?', '')
+        decodedLine = decodedLine.replace('"', '')
+        decodedLine = decodedLine.replace('\'', '')
+        decodedLine = decodedLine.replace('yes', 'true')
+        decodedLine = decodedLine.replace('no', 'false')
+        decodedLine = decodedLine.lower()
+        print(decodedLine.strip())
+        bumps_file.write(decodedLine.strip() + '\n')
+    bumps_file.close()
